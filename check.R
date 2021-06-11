0942b4a38c0835e39d0a041aaa9bb1cff2ecaddc
Nan-Hung Hsieh <nan_1003@hotmail.com>
2021-06-11 13:02:03 -0700

    i=5000

--- check.R
+++ check.R
@@ -1,3 +1,10 @@
+system("makemcsim perc.model")
+system.time(system("./mcsim.perc -i 100 perc.mcmc.in"))
+
 df <- read.delim("perc.mcmc.out")
+plot(df$iter, df$Km.1., type ="l")
+plot(df$iter, df$sc_Vmax.1., type ="l")
+
 post.df <- tail(df, 500)
 plot(post.df$sc_Vmax.1., post.df$Km.1.)
+
